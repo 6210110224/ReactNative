f186e6eaaa80cff088ca780095924755fd4e9d39
6210110224 <6210110224@psu.ac.th>
2021-07-12 10:50:54 +0700

    Components and Image Background

--- App.js
+++ App.js
@@ -4,19 +4,22 @@ import { StyleSheet, View } from 'react-native';
 import Weather from './components/Weather';
 
 export default function App() {
-   return (
-    <View style={styles.container}>
-      <Weather zipCode="90110"/>
-      <StatusBar style="auto" />
-    </View>
+    return ( <
+        View style = { styles.container } >
+        <
+        Weather zipCode = "90110" / >
+        <
+        StatusBar style = "auto" / >
+        <
+        /View>
     );
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
- 
+
 });
--- components/Weather.js
+++ components/Weather.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import { Text } from 'react-native'
+import React, { useState } from 'react';
+import { Text, ImageBackground, StyleSheet } from 'react-native';
+import Forecast from './Forecast';
 
 export default function Weather(props) {
-    return (
-    <Text>{props.zipCode}</Text>
+    const [forecastInfo, setForecastInfo] = useState({
+
+        main: '-',
+        description: '-',
+        temp: 0
+    })
+
+    return ( <
+        View >
+        <
+        ImageBackground source = { require('../bg.jpg') }
+        styles = { styles.backdrop } >
+        <
+        Forecast {...forecastInfo }
+        /> <
+        /ImageBackground> <
+        /View>
     );
-   }
-   
+}
+const styles = StyleSheet.create({
+    backdrop: {
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+    }
+})
