362d2f584c44b18f426be86c985894e2df58cd24
6210110224 <6210110224@psu.ac.th>
2021-07-20 14:48:47 +0700

    commit test

--- components/Weather.js
+++ components/Weather.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Text, ImageBackground, StyleSheet, View } from 'react-native';
+import {Text, ImageBackground, StyleSheet, View } from 'react-native';
 import Forecast from './Forecast';
 
 export default function Weather(props) {
@@ -9,24 +9,22 @@ export default function Weather(props) {
         description: '-',
         temp: 0
     })
-    return ( <
-        View >
-        <
-        ImageBackground source = { require('../bg.jpg') }
-        style = { styles.backdrop } >
-        <
-        Text > Zip Code < /Text> <
-        Text > { props.zipCode } < /Text> <
-        Forecast {...forecastInfo }
-        /> <
-        /ImageBackground> <
-        /View>
+    return (
+        <View>
+            <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+                <Text>Zip Code</Text>
+                <Text>{props.zipCode}</Text>
+                <Forecast {...forecastInfo} />
+            </ImageBackground>
+        </View>
     );
 }
 const styles = StyleSheet.create({
     backdrop: {
+        flexDirection: 'column',
         alignItems: 'center',
         width: '100%',
         height: '100%'
     },
 });
+   
